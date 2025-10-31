63b7923523959d66e2701d8d06fbabd78528fac6
Bui Quoc Tuan <tusela022@gmail.com>
2025-11-01 04:50:49 +0700

    Them xem , xoa , update

--- identity-service/identity-service/src/main/java/com/devteria/identity_service/service/UserService.java
+++ identity-service/identity-service/src/main/java/com/devteria/identity_service/service/UserService.java
@@ -1,10 +1,13 @@
 package com.devteria.identity_service.service;
 
 import com.devteria.identity_service.dto.request.UserCreationRequest;
+import com.devteria.identity_service.dto.request.UserUpdateRequest;
 import com.devteria.identity_service.entity.User;
 import com.devteria.identity_service.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -21,5 +24,25 @@ public class UserService {
         users.setDob(request.getDob());
 
        return userRepository.save(users);
+    }
+    public List<User> getUsers()
+    {
+        return userRepository.findAll();
+    }
+    public User getUserById(String id)
+    {
+        return userRepository.findById(id).get();
+    }
+    public User userUpdate(String userID, UserUpdateRequest request)
+    {
+     User user = getUserById(userID);
+     user.setFirstName(request.getFirstName());
+     user.setLastName(request.getLastName());
+     user.setDob(request.getDob());
+     return userRepository.save(user);
+    }
+    public void  deleteUserById(String id)
+    {
+        userRepository.deleteById(id);
     }
 }
